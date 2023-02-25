3268156b8c2f99d6d7eb8547ef138f8a701e09d9
emre gider <yegider7@gmail.com>
2023-02-25 10:08:23 +0300

    Emre değiştirdi.

--- src/Program.java
+++ src/Program.java
@@ -2,5 +2,6 @@ public class Program {
     public static void main(String[] args) {
         System.out.println("Yeni proje'den herkese merhaba!");
         System.out.println("-------------------------------");
+        System.out.println("Her şey güzel olacak");
     }
 }
